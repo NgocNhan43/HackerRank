7e23ad9c8b1f17a4cfe4e5dae86fcaaf0091beb1
NgocNhan43 <nguyenngocnhan442002@gmail.com>
2024-12-26 17:10:13 +0700

    Firt commit

---
+++ src/ReadLine.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class ReadLine {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int i = sc.nextInt();
+        double d = sc.nextDouble();
+        String s = sc.nextLine();
+        //
+        // Write your code here.
+
+        System.out.println("String: " + s);
+        System.out.println("Double: " + d);
+        System.out.println("Int: " + i);
+    }
+
+
+
+}
