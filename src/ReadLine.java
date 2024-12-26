e4a13b47158a749e9e1ad54471102117b96a94df
NgocNhan43 <nguyenngocnhan442002@gmail.com>
2024-12-26 17:11:59 +0700

    Firt commit

--- src/ReadLine.java
+++ src/ReadLine.java
@@ -14,6 +14,7 @@ public class ReadLine {
         System.out.println("String: " + s);
         System.out.println("Double: " + d);
         System.out.println("Int: " + i);
+        System.out.println("Int: " + i);
     }
 
 
